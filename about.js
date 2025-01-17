4d6078ed3b84cd567dceb9c0473e1f2b46c48aa6
HITESHRATHI2003 <162580749+HITESHRATHI2003@users.noreply.github.com>
2025-01-17 11:20:46 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,3 @@
+document.querySelector('.resume-btn').addEventListener('click', function() {
+    window.open('resume.pdf', '_blank');  // Replace 'resume.pdf' with the path to your resume file
+});
